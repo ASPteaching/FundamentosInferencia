b07885419c67658a6c7f2b91d51772cfe36090e7
ASPteaching <asanchez@ub.edu>
2026-01-05 10:02:57 +0100

    Bibliografia

--- renderBook.R
+++ renderBook.R
@@ -1,1 +1,2 @@
-bookdown::render_book("index.Rmd",output_format = c("bookdown::pdf_book"))
+bookdown::render_book("index.Rmd")
+bookdown::render_book("index.Rmd",output_format = c("bookdown::gitbook", "bookdown::pdf_book"))
